acc176f0ce7b2feeb2c6b04b665377f383bd1220
Usamaa1 <43386888+Usamaa1@users.noreply.github.com>
2024-03-30 17:59:28 +0500

    WPF Lecture 1

---
+++ 220G2-wpf/220G2-wpf/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _220G2_wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void prodName_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if(prodName.Text == "Enter Product Name")
+            {
+                prodName.Text = "";
+                prodName.Foreground = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+            }
+        }
+
+        private void prodName_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (prodName.Text == "")
+            {
+                prodName.Text = "Enter Product Name";
+                prodName.Foreground = new SolidColorBrush(Color.FromRgb(127, 140, 141));
+            }
+        }
+
+        private void prodPrice_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (prodPrice.Text == "Enter Price")
+            {
+                prodPrice.Text = "";
+                prodPrice.Foreground = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+            }
+        }
+
+        private void prodPrice_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (prodPrice.Text == "")
+            {
+                prodPrice.Text = "Enter Price";
+                prodPrice.Foreground = new SolidColorBrush(Color.FromRgb(127, 140, 141));
+            }
+
+        }
+
+        private void prodBarcode_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (prodBarcode.Text == "Enter Barcode")
+            {
+                prodBarcode.Text = "";
+                prodBarcode.Foreground = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+            }
+
+        }
+
+        private void prodBarcode_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (prodBarcode.Text == "")
+            {
+                prodBarcode.Text = "Enter Barcode";
+                prodBarcode.Foreground = new SolidColorBrush(Color.FromRgb(127, 140, 141));
+            }
+        }
+
+        private void addBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Product Name: {prodName.Text}\n Product Price: {prodPrice.Text}\n Product Barcode: {prodBarcode.Text}");
+        }
+    }
+}
